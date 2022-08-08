fe2aa888715d7e35402df5ee3631c52e12807392
Thavananthan <nnanthu49@gmail.com>
2022-08-08 23:27:02 +0530

    table view and application form done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Profile from "./pages/profile";
 import Job from "./pages/job";
 import Education from "./pages/education";
 import User from "./pages/user";
+import Application from "./pages/application";
+import Table from "./pages/tabel";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/job" element={<Job />} exact />
         <Route path="/edu" element={<Education />} exact />
         <Route path="/user" element={<User />} exact />
+        <Route path="/app" element={<Application />} exact />
+        <Route path="/tab" element={<Table />} exact />
       </Routes>
     </div>
   );
---
+++ src/pages/application/index.js
@@ -0,0 +1,16 @@
+import "./style.css";
+import Applications from "../../components/application/Application";
+export default function Application() {
+  return (
+    <div className="profile">
+      <div className="profile_top">
+        <div className="profile_container">
+          <div className="login">
+            <h1>TIME HIRE THE BEST</h1> <br />
+            <Applications />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
